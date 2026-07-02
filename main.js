e0ff96af273731fe5e9ae038b33bfe15124c046d
Tommaso <tommasocostanza7@gmail.com>
2026-07-02 17:47:02 +0200

    Update 404: change Voyager rotation to X-axis, make credit text lowercase, raise elements and Back home button

--- main.js
+++ main.js
@@ -1296,7 +1296,7 @@ function load404Scene(canvasId) {
   function animate404() {
     requestAnimationFrame(animate404);
     if (voyager) {
-      voyager.rotation.y += 0.002;
+      voyager.rotation.x += 0.002;
       voyager.position.y = Math.sin(Date.now() * 0.001) * 10;
     }
     renderer.render(scene, camera);
